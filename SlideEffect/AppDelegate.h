e28190ecb68f55b178d46725ff859d840da179bd
Khairul Islam <khairulislam@Khairuls-MacBook-Pro.local>
2016-10-22 22:51:54 +0600

    Initial Commit

---
+++ SlideEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideEffect
+//
+//  Created by Khairul Islam on 10/22/16.
+//  Copyright © 2016 Khairul Islam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlideEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideEffect
+//
+//  Created by Khairul Islam on 10/22/16.
+//  Copyright © 2016 Khairul Islam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
